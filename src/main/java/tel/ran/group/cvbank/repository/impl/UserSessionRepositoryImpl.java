c1d438e7a8ed89bf51df1161f3983c9d60c7dc7b
Tatyana Gibadulina <Tank232@gmail.com>
2018-10-07 07:11:47 +0300

    no_andestend_commit

---
+++ src/main/java/tel/ran/group/cvbank/repository/impl/UserSessionRepositoryImpl.java
@@ -0,0 +1,55 @@
+package tel.ran.group.cvbank.repository;
+
+import org.springframework.stereotype.Repository;
+import tel.ran.group.cvbank.model.entity.User;
+import tel.ran.group.cvbank.model.entity.UserSession;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class UserSessionRepository {
+
+    private static Map<String, UserSession> sessions = new HashMap<>();
+
+    public void create(String sessionId, User user) {
+        UserSession userSession = UserSession
+                .builder()
+                .sessionId(sessionId)
+                .user(user)
+                .isValid(true)
+                .build();
+        sessions.putIfAbsent(sessionId, userSession);
+
+    }
+
+    public UserSession getBySessionId(String sessionId) {
+        UserSession userSession = sessions.get(sessionId);
+        if (userSession == null || !userSession.getIsValid()) {
+            return null;
+        }
+
+
+        return userSession;
+    }
+
+    public String getToken(User user) {
+        String token = sessions.entrySet()
+                .stream()
+                .filter(value -> value.getValue().getUser().equals(user))
+                .map(Map.Entry::getKey)
+                .findFirst().get();
+        return token;
+
+    }
+
+    public void invalidateSession(String sessionId) {
+        UserSession userSession = sessions.get(sessionId);
+        if (userSession == null) {
+            return;
+        }
+        userSession.setIsValid(false);
+        sessions.put(sessionId, userSession);
+    }
+}
